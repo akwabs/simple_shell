7c328e9245bc3a2b2f7d5e31a91c61f68addf8da
akwabs <112529868+akwabs@users.noreply.github.com>
2022-11-15 03:22:01 +0000

    Create free_loop.c

---
+++ free_loop.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+/**
+*free_loop - function for free double pointer array of pointers
+*@arr: double pointer array of pointers
+*/
+void free_loop(char **arr)
+{
+	int len;
+
+	for (len = 0; arr[len]; len++)
+		free(arr[len]);
+	free(arr);
+}
+
+/**
+ *free_list - frees a listint_t list
+ *@head: head of a listint_t list
+ */
+void free_list(list_p *head)
+{
+	list_p *prev;
+
+	while (head)
+	{
+		free(head->dir);
+		prev = head;
+		head = head->next;
+		free(prev);
+	}
